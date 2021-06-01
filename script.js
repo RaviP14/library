c6b79fd61a1b4c5ef029d3a827d07bafde2c2f86
RaviP14 <ravi.patel14@outlook.com>
2021-06-01 13:00:48 +0100

    table added to page.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function book(title, author, pages, read) {
 function addBookToLibrary() {
     //create new book (Object.create function)
     //push book into the array
-    input1 = prompt('Enter title',) //only input for title
+    input1 = prompt('Enter title',)
     input2 = prompt('Enter author',)
     input3 = prompt('Enter pages',)
     input4 = prompt('Enter reading status(read/not read)',)
@@ -22,5 +22,26 @@ function addBookToLibrary() {
     console.log(myLibrary)
 }
 
-//let theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
-//console.log(theHobbit)
+let theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
+myLibrary.push(theHobbit)
+
+let gameOfThrones = new book('A Game Of Thrones', 'George R. R. Martin', 694, 'not read yet')
+myLibrary.push(gameOfThrones)
+
+let table = document.createElement('table');
+
+function newEntry() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        row = table.insertRow(myLibrary[i])
+        for (let j = 0; j < Object.keys(myLibrary[i]).length - 1; j++) {
+            let val = row.insertCell()
+            val.textContent = Object.values(myLibrary[i])[j]
+        }
+    }
+}
+
+newEntry()
+
+let mBody = document.querySelector('body');
+
+mBody.appendChild(table);
