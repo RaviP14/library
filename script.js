e988c4ca6b6b6c1cc7923bafac5bbcb1168d8ffe
RaviP14 <ravi.patel14@outlook.com>
2021-06-02 14:01:01 +0100

    new book form added

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ let x = 2
 function addBookToLibrary() {
     //create new book (Object.create function)
     //push book into the array
-    input1 = prompt('Enter title',)
-    input2 = prompt('Enter author',)
-    input3 = prompt('Enter pages',)
-    input4 = prompt('Enter reading status(read/not read)',)
+    //input1 = prompt('Enter title',)
+    //input2 = prompt('Enter author',)
+    //input3 = prompt('Enter pages',)
+    //input4 = prompt('Enter reading status(read/not read)',)
     let books = new book(input1, input2, input3, input4)
     myLibrary.push(books)
     console.log(myLibrary)
@@ -55,5 +55,55 @@ newBookBtn.textContent = 'New Book';
 mBody.appendChild(newBookBtn)
 
 newBookBtn.addEventListener('click', () => {
-    addBookToLibrary()
+    setNewBook()
 })
+
+//form input
+
+function setNewBook () {
+    let form = document.createElement('form')
+    mBody.appendChild(form)
+
+    let inputTitle = document.createElement('input');
+    inputTitle.name = 'title';
+    inputTitle.placeholder = 'Title'
+    inputTitle.autocomplete = 'off'
+    form.appendChild(inputTitle)
+
+    let inputAuthor = document.createElement('input');
+    inputAuthor.name = 'author'
+    inputAuthor.placeholder = 'Author'
+    inputAuthor.autocomplete = 'off'
+    form.appendChild(inputAuthor);
+
+    let inputPages = document.createElement('input');
+    inputPages.name = 'pages'
+    inputPages.placeholder = 'Total Pages'
+    inputPages.autocomplete = 'off'
+    form.appendChild(inputPages);
+
+    let inputStatus = document.createElement('input');
+    inputStatus.name = 'status'
+    inputStatus.placeholder = 'Read Status'
+    inputStatus.autocomplete = 'off'
+    form.appendChild(inputStatus);
+
+    let submit = document.createElement('button');
+    submit.name = 'submit'
+    submit.className = 'submitForm'
+    submit.textContent = 'Submit'
+    form.appendChild(submit);
+
+    submit.addEventListener('click', (e) => {
+        //submit values eg transfer inputTitle.value to new book object
+        input1 = inputTitle.value.slice()
+        input2 = inputAuthor.value.slice()
+        input3 = parseFloat(inputPages.value.slice())
+        input4 = inputStatus.value.slice()
+        addBookToLibrary(input1, input2, input3, input4)
+        //book dissapeares straight after
+        console.log(myLibrary) // book dissapears after event listeners ends
+        e.preventDefault()
+        form.reset();
+    })
+}
